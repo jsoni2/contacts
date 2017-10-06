b8dfd6272758e27c77dbd96b9067aaff0fd696c5
Janak Soni <jakzi7005@gmail.com>
2017-10-06 13:48:34 -0700

    Remove starter content

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class ContactList extends React.Component {
-  render() {
-    const people = this.props.contacts
-
-    return <ol>
-      {people.map(person => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
-  }
-}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: 'Janak' },
-          { name: 'Jake' },
-          { name: 'Jon' }
-        ]}/>
-        <ContactList contacts={[
-          { name: 'Dean' },
-          { name: 'Sam' },
-          { name: 'Cass' }
-        ]}/>
-      </div>
-    );
+    
   }
 }
 
